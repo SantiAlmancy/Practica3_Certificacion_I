a198618136cfbaaeb5e09bf5ced7ef874e6f7c87
Santiago Becerra <santialmancy@gmail.com>
2023-05-02 15:11:57 -0400

    P3-001 Creation of cRud operation, functions GetById and GetAll.

--- CoreLogic/Managers/PatientManager.cs
+++ CoreLogic/Managers/PatientManager.cs
@@ -1,8 +1,51 @@
 using UPB.CoreLogic.Models;
+using System.Xml.Serialization;
 
 namespace UPB.CoreLogic.Managers;
 
 public class PatientManager
 {
-    
+    private string filePath;
+    public PatientManager(string _filePath)
+    {
+        filePath = _filePath;
+    }
+
+    // READ R
+    // GET BY ID
+
+    public Patient GetById(int ci)
+    {
+        if (ci < 0)
+        {
+            throw new Exception("INVALID CI");
+        }
+
+        List<Patient> patients = GetAll();
+        Patient patient = patients.Find(p => p.CI == ci);
+
+        if (patient == null)
+        {
+            throw new Exception("Patient not found");
+        }
+
+        return patient;
+    }
+
+    // GET ALL
+
+    public List<Patient> GetAll()
+    {
+        if (!File.Exists(filePath))
+        {
+            return new List<Patient>();
+        }
+
+        XmlSerializer serializer = new XmlSerializer(typeof(List<Patient>));
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            return (List<Patient>)serializer.Deserialize(reader);
+        }
+    }
 }
