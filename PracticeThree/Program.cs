80299fa5c259146a3777ae2869da17fb050b1724
Santiago Becerra <santialmancy@gmail.com>
2023-05-04 09:36:00 -0400

    P3-001 Modification in program.cs to have dynamic title swagger.

--- PracticeThree/Program.cs
+++ PracticeThree/Program.cs
@@ -1,5 +1,6 @@
 using UPB.PracticeThree.Middlewares;
 using UPB.CoreLogic.Managers;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,24 @@ builder.Services.AddTransient<PatientManager>(sp => new PatientManager("C:\\User
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var configurationBuilder = new ConfigurationBuilder()
+        .SetBasePath(builder.Environment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+IConfiguration Configuration = configurationBuilder.Build();
+string siteTitle = Configuration.GetSection("Title").Value;
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = siteTitle
+    });
+});
 
 var app = builder.Build();
 
@@ -22,12 +40,16 @@ app.UseGlobalExceptionHandler();
 // app.UseCors();
 // app.UseAuthentication();
 // app.UseAuthorization();
-
+/*
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
